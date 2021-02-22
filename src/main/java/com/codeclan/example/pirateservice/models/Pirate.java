bd4f41893db7f174cf0f2385be984ade92048500
Stanhoucke <stanhoucke@gmail.com>
2021-02-22 17:14:25 +0000

    onetomany and manytomany relationships added

--- src/main/java/com/codeclan/example/pirateservice/models/Pirate.java
+++ src/main/java/com/codeclan/example/pirateservice/models/Pirate.java
@@ -1,12 +1,15 @@
 package com.codeclan.example.pirateservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="pirates")
 public class Pirate {
-
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,10 +25,36 @@ public class Pirate {
     @Column(name = "age")
     private int age;
 
-    public Pirate(String firstName, String lastName, int age) {
+    @ManyToOne
+    @JsonIgnoreProperties({"pirates"})
+    @JoinColumn(name = "ship_id", nullable = false)
+    private Ship ship;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"pirates"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "pirates_raids",
+            joinColumns = { @JoinColumn(
+                    name = "pirate_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "raid_id",
+                    nullable = false,
+                    updatable = false)
+            }
+
+    )
+    private List<Raid> raids;
+
+    public Pirate(String firstName, String lastName, int age, Ship ship) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.ship = ship;
+        this.raids = new ArrayList<Raid>();
     }
 
 
@@ -60,5 +89,25 @@ public class Pirate {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    public List<Raid> getRaids() {
+        return raids;
+    }
+
+    public void setRaids(List<Raid> raids) {
+        this.raids = raids;
+    }
+
+    public void addRaid(Raid raid){
+        this.raids.add(raid);
     }
 }
--- src/main/java/com/codeclan/example/pirateservice/models/Ship.java
+++ src/main/java/com/codeclan/example/pirateservice/models/Ship.java
@@ -1,5 +1,7 @@
 package com.codeclan.example.pirateservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,7 @@ public class Ship {
     @Column(name = "name")
     private String name;
 
+    @JsonIgnoreProperties({"ship"})
     @OneToMany(mappedBy="ship")
     private List<Pirate> pirates;
 
